f33de7657a2dcf4b4c18ec93b0e23cb114edbf0a
enasbo1 <irenee.du.gardin@gmail.com>
2023-10-12 08:42:46 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.time.Instant;
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+    }
+}
